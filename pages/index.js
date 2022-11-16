220fc552b63494af9aeaf479a72e2f24fff36327
chowdhuryy-ifty <mchowdhuryy@binaryquest.com>
2022-11-16 09:37:33 +0600

    router replace command

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 import styles from '../styles/Home.module.css'
 import Link from "next/link";
+import {useRouter} from "next/router";
 
 export default function Home() {
+    const router = useRouter()
+    const handleClick =() => {
+        console.log('Placing your order')
+        router.replace(`/product`)
+    }
     return (
         <div className={styles.container}>
             <h1>Home Page</h1>
@@ -12,6 +18,10 @@ export default function Home() {
             <Link href="/product">
                 <>Product</>
             </Link>
+            <br/>
+            <button onClick={handleClick}>
+                Place Order
+            </button>
         </div>
     )
 }
