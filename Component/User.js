ae34adecea826cecc24c025948d5d14921421e53
chowdhuryy-ifty <mchowdhuryy@binaryquest.com>
2022-11-16 16:56:21 +0600

    some user details add to render

--- Component/User.js
+++ Component/User.js
@@ -2,7 +2,9 @@ export default function User({user}) {
     return (
         <ul>
             <p>{user.name}</p>
+            <p>{user.username}</p>
             <p>{user.email}</p>
+            <p>{user.address.street}</p>
             <br/>
         </ul>
     )
