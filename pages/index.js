239f7a45ab16f2400368e8fcc016081338d8a118
chowdhuryy-ifty <mchowdhuryy@binaryquest.com>
2022-11-29 16:02:01 +0600

    app layout

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import styles from '../styles/Home.module.css'
 import Link from "next/link";
-import {useRouter} from "next/router";
 
 export default function Home() {
-    const router = useRouter()
 
     return (
         <div className={styles.container}>
+            <button className='btn btn-primary'>Yooo mate</button>
+            <h2>Testing h2</h2>
             <h1>Home Page</h1>
             <Link href="/about">
                 <>about</>
