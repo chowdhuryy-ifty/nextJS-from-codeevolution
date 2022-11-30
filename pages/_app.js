7b0903092276abe7d491854850ee1d94a78c225c
chowdhuryy-ifty <mchowdhuryy@binaryquest.com>
2022-11-30 12:07:00 +0600

    header and footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Header from "../Component/Header";
+import Footer from "../Component/Footer";
 
 function MyApp({Component, pageProps}) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Header/>
+            <Component {...pageProps} />
+            <Footer/>
+        </>
+    )
 }
 
 export default MyApp
